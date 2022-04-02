ee60f39f1563452770f84d2db08f767b451f1826
Zzznorlax <legohulk27@gmail.com>
2022-04-02 22:46:02 +0800

    feat(flat shading): finish flat shader, scaling, rotation and translation

--- src/shader.js
+++ src/shader.js
@@ -8,6 +8,7 @@ var shaders = shaders || {
 
 shaders.other.vertexShader = `
 attribute vec3 aVertexPosition;
+attribute vec3 aVertexNormal;
 attribute vec3 aFrontColor;
 
 uniform mat4 uMVMatrix;
@@ -28,5 +29,76 @@ varying vec4 fragcolor;
 
 void main(void) {
     gl_FragColor = fragcolor;
+}
+`
+
+shaders.flat.vertexShader = `
+precision mediump float;
+
+attribute vec3 aVertexPosition;
+attribute vec3 aVertexNormal;
+attribute vec3 aFrontColor;
+
+uniform mat4 uMVMatrix;
+uniform mat4 uPMatrix;
+uniform mat3 uNMatrix;
+
+vec4 fragColor;
+
+varying vec4 vFragcolor;
+varying vec3 vNormalDirection;
+varying vec3 vVertexPosition;
+varying vec3 vLightDirection;
+
+uniform vec3 uPointLightingLocation;
+
+void main(void) {
+    gl_Position = uPMatrix * uMVMatrix * vec4(aVertexPosition, 1.0);
+
+    vNormalDirection = normalize(uNMatrix * aVertexNormal);
+    vVertexPosition = (uMVMatrix * vec4(aVertexPosition, 1.0)).xyz;
+
+    vLightDirection = normalize(uPointLightingLocation - (uMVMatrix * vec4((aVertexPosition), 1.0)).xyz);
+
+    fragColor = vec4(aFrontColor, 1.0);
+    vFragcolor = fragColor;
+}
+`
+
+shaders.flat.fragmentShader = `
+#extension GL_OES_standard_derivatives : enable
+
+precision mediump float;
+
+varying vec4 vFragcolor;
+varying vec3 vLightDirection;
+varying vec3 vNormalDirection;
+varying vec3 vVertexPosition;
+
+uniform float uMaterialShininess;
+
+uniform vec3 uAmbientColor;
+uniform vec3 uPointLightingSpecularColor;
+uniform vec3 uPointLightingDiffuseColor;
+
+void main(void) {
+	vec3 dx = dFdx(vVertexPosition);
+	vec3 dy = dFdy(vVertexPosition);
+	vec3 tn = normalize(cross(dx, dy));
+
+    vec3 eyeDirection = normalize(-vVertexPosition.xyz);
+    vec3 lightWeighting = vec3(0.0, 0.0, 0.0);
+
+    vec3 reflectionDirection = reflect(-vLightDirection, tn);
+
+    float specularLightWeighting = pow(max(dot(reflectionDirection, eyeDirection), 0.0), uMaterialShininess);
+
+    float diffuseLightWeighting = max(dot(tn, vLightDirection), 0.0);
+
+    lightWeighting = uAmbientColor
+    + uPointLightingSpecularColor * specularLightWeighting
+    + uPointLightingDiffuseColor * diffuseLightWeighting;
+
+	gl_FragColor = vec4(vFragcolor.rgb * lightWeighting, vFragcolor.a);
 }
 `
